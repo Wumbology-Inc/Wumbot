c01e1ea7fe2788e1b559ec7cbc72c79ea7716b0c
sco1 <sco1@users.noreply.github.com>
2018-01-14 20:11:22 +1000

    Add basic functionality, helper functions
    Add check for bot as author to avoid self-replying
    Add hardcoded DM kill command
    Add login credentials loading from input JSON file
    Update login with silly game name
    
    Add helper to check if the User is me
    Add helper to check if the channel is a DM channel

--- wumbot.py
+++ wumbot.py
@@ -1,14 +1,56 @@
 import json
+
 import discord
 
-client = discord.Client()
 
-@client.event
-async def on_ready():
-    print(f'Logged in as {client.user}')
+class WumbotClient(discord.Client):
+    async def on_ready(self):
+        print(f'Logged in as {self.user}')
+
+    async def on_message(self, message):
+        # Avoid self-replies
+        if message.author.id == self.user.id:
+            return
+
+        # Hardcode a 'kill' command in a DM to the bot from me
+        if isDM(message.channel) and isELA(message.author):
+            if message.content == 'kill':
+                print('ELA killed me')
+                await self.close()
+
+def isELA(user):
+    """
+    Check to see if the input User's ID matches my ID
+    """
+    ELAid = 129606635545952258
+    if user.id == ELAid:
+        return True
+    else:
+        return False
 
 
-with open('credentials.JSON', mode='r') as fID:
-    credentials = json.load(fID)
+def isDM(channel):
+    """
+    Check to see if a channel is a DM
 
-client.run(credentials['TOKEN'])
+    A DM is either an instance of DMChannel or GroupChannel
+    """
+    if isinstance(channel, discord.TextChannel):
+        return False
+    else:
+        return True
+
+def loadCredentials(sourceJSON):
+    """
+    Load login credentials from the input JSON file
+    """
+    with open(sourceJSON, mode='r') as fID:
+        credentials = json.load(fID)
+
+    return credentials
+
+credentials = loadCredentials('credentials.JSON')
+if credentials:
+    wumbogame = discord.Game(name='The Game of Wumbo')
+    client = WumbotClient(game=wumbogame)
+    client.run(credentials['TOKEN'])
