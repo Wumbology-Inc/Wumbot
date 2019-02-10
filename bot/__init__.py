c85560a05dcc5b2f3f13a8bcfecf0443ea3469f2
sco1 <sco1.git@gmail.com>
2019-02-10 15:46:47 -0500

    Add logzio logging

--- bot/__init__.py
+++ bot/__init__.py
@@ -1,15 +1,24 @@
 import logging
+import logging.config
 import time
+from pathlib import Path
 
 
-logging.Formatter.converter = time.gmtime  # Force UTC
+LOGZIO_CONF_PATH = Path('./logzio.conf')
 
-logformat = "%(asctime)s %(levelname)s:%(module)s:%(message)s"
-dateformat = "%Y-%m-%d %H:%M:%S"
-logging.basicConfig(
-    filename="./log/wumbot.log",
-    filemode="a",
-    level=logging.INFO,
-    format=logformat,
-    datefmt=dateformat,
-)
+if LOGZIO_CONF_PATH.exists():
+    logging.config.fileConfig('logzio.conf')
+    logger = logging.getLogger('LogzioLogger')
+else:
+    # Default to local logging
+    logging.Formatter.converter = time.gmtime  # Force UTC
+
+    logformat = "%(asctime)s %(levelname)s:%(module)s:%(message)s"
+    dateformat = "%Y-%m-%d %H:%M:%S"
+    logging.basicConfig(
+        filename="./log/wumbot.log",
+        filemode="a",
+        level=logging.INFO,
+        format=logformat,
+        datefmt=dateformat,
+    )
